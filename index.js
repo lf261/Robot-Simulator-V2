c2cc123146e1d28e31ddc83045f29c105a38776c
lf261 <luis.fernandez2093@gmail.com>
2020-07-15 10:28:47 -0400

    dq stand up

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+
 document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  let commandsByZara = document.getElementById("moves-container")
+
+  let monster = document.querySelector("#robot")
+
+        document.addEventListener("keydown", function(e){
+           const direction  = e.key.slice(5)
+           console.log(direction)
+
+            let listElement = document.createElement("li")
+
+            listElement.textContent = direction
+            commandsByZara.append(listElement)
+
+        })
+
+    let button = document.getElementById("move-button") 
+
+    button.addEventListener("click", function(e){
+      commandsByZara.children
+
+    })
+
+
+
+
 })
